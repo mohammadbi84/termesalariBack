108cb7b139c40ac84a9a6d0c4514cb0e44b95b0e
mohammadbi84 <em.bi.4884@gmail.com>
2026-02-23 20:01:00 +0330

    compare page changed

--- app/Http/Controllers/CompareController.php
+++ app/Http/Controllers/CompareController.php
@@ -8,7 +8,8 @@ class CompareController extends Controller
 {
 	public function index()
 	{
-		return view("compare.index");
+		return view("compare.compare");
+		// return view("compare.index");
 	}
 
     public function add(Request $request)
